bc2864733dadffe6d09560fae430ac461bcd7771
Mike Risse <rissem@gmail.com>
2010-11-26 06:24:30 -0800

    add a couple 'hello world' tests

---
+++ miniMe/src/test/java/com/jukejuice/HelloWorldTest.java
@@ -0,0 +1,18 @@
+package com.jukejuice;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+	
+	@Test
+	public void sampleTest() {
+		assert 1 == 1;
+	}
+	
+	@Test
+	public void sampleTest2() {
+		assert 2 == 2;
+	}
+}
+
+   
