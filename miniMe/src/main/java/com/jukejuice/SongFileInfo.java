faa26ae43eb326e2dd861e61b1e76b33ae178c89
Mike Risse <rissem@gmail.com>
2011-01-30 20:01:38 -0800

    catch all SongInfo exceptions (problems parsing video files, poorly formatted mp3s, etc)

--- miniMe/src/main/java/com/jukejuice/SongFileInfo.java
+++ miniMe/src/main/java/com/jukejuice/SongFileInfo.java
@@ -1,17 +1,12 @@
 package com.jukejuice;
 
 import java.io.File;
-import java.io.IOException;
 
 import org.apache.log4j.Logger;
 import org.jaudiotagger.audio.AudioFile;
 import org.jaudiotagger.audio.AudioFileIO;
 import org.jaudiotagger.audio.AudioHeader;
-import org.jaudiotagger.audio.exceptions.CannotReadException;
-import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException;
-import org.jaudiotagger.audio.exceptions.ReadOnlyFileException;
 import org.jaudiotagger.tag.Tag;
-import org.jaudiotagger.tag.TagException;
 
 public class SongFileInfo {
 	private static final Logger log = Logger.getLogger(SongFileInfo.class);
@@ -27,15 +22,7 @@ public class SongFileInfo {
 	{
 		try {
 			f = AudioFileIO.read(new File(filename));
-		} catch (CannotReadException e) {
-			log.error("", e);
-		} catch (IOException e) {
-			log.error("", e);
-		} catch (TagException e) {
-			log.error("", e);
-		} catch (ReadOnlyFileException e) {
-			log.error("", e);
-		} catch (InvalidAudioFrameException e) {
+		} catch (Exception e) {
 			log.error("", e);
 		}
 		header = f.getAudioHeader();
